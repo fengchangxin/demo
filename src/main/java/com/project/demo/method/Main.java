cefc19f98a3fc60b5754bdb939f7111ef67e16c8
fengchangxin <8698027feng>
2019-12-16 11:03:16 +0800

    方法引用

--- src/main/java/com/project/demo/method/Main.java
+++ src/main/java/com/project/demo/method/Main.java
@@ -6,27 +6,46 @@ package com.project.demo.method;
  * @description:
  */
 
-import com.project.demo.lambda.IMath;
-import com.project.demo.lambda.Math;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 方法引用，也是lambda，只调用其他的一个方法时使用，需要返回类型和参数对应
  */
 public class Main {
     public static void main(String[] args) {
-        Study s1 = (name) -> new Student(name);
+        List<String> list = Arrays.asList("123", "456", "789");
+        Map<String, String> map = new HashMap<>(3);
+        map.put("123", "123");
+        map.put("456", "456");
+        map.put("789", "789");
+        List<Student> studentList = Arrays.asList(new Student(), new Student("fff"), new Student("dddd"));
 
-        //构造方法
-        Study s2 = Student::new;
+        //构造方法，调用无参构造方法,Class::new
+        Student s1 = Student.create(Student::new);
 
-        //静态方法
-        Study s3 = Student::create;
+        //调用有参构造方法，参数与接口方法一致
+        Study study = Student::new;
+        study.study("e234");
 
-        //对象方法
-        Student student = new Student();
-        Study s4 = student::get;
+        //静态方法调用，参数根据上下文确定，需要类型数量对应，Class::static_method
+        //是lambad，原样
+        list.forEach(s -> {
+            Student.follow(s);
+        });
+        list.forEach(Student::follow);
+        studentList.forEach(Student::follow);
+        map.forEach(Student::follow);
+        //System.out.println(Student::follow);
 
+        //对象方法调用，Class::method，只接受无参方法
+        //list.forEach(Student::getName);
+        studentList.forEach(Student::getName);
 
-
+        //对象方法调用，instant::method，参数类型对应
+        list.forEach(s1::getName);
+        studentList.forEach(s1::getName);
     }
 }
--- src/main/java/com/project/demo/method/Student.java
+++ src/main/java/com/project/demo/method/Student.java
@@ -1,37 +1,50 @@
 package com.project.demo.method;
 
+import java.util.function.Supplier;
+
 /**
  * @author: fcx
  * @date: 2019-11-19 23:08
  * @description:
  */
 public class Student {
-    private String name;
+    public static Student create(final Supplier<Student> supplier) {
+        return supplier.get();
+    }
 
     public Student() {
+        System.out.println("无参构造方法");
     }
 
     public Student(String name) {
-        this.name = name;
+        System.out.println("有参构造方法：" + name);
     }
 
-    /**
-     * 静态方法
-     *
-     * @param name
-     * @return
-     */
-    public static Student create(String name) {
-        return new Student(name);
+    public static void follow() {
+        System.out.println("无参follow");
     }
 
-    /**
-     * 对象方法
-     *
-     * @param s
-     * @return
-     */
-    public Student get(String s) {
-        return new Student(s);
+    public static void follow(String name) {
+        System.out.println("有参follow：" + name);
+    }
+
+    public static void follow(Student student) {
+        System.out.println("有参follow：" + student);
+    }
+
+    public static void follow(String key, String value) {
+        System.out.println("有参follow：" + key + ":" + value);
+    }
+
+    public void getName() {
+        System.out.println("无参getName");
+    }
+
+    public void getName(String name) {
+        System.out.println("有参getName：" + name);
+    }
+
+    public void getName(Student student) {
+        System.out.println("有参getName：" + student);
     }
 }
